346539b113dbd61b77c39d5807b5f2c10662f316
Ben Bubnick <bfbubnic@us.ibm.com>
2019-10-24 14:06:39 -0400

    Added some comments to the code

--- test_scripts/Introduction_perl.pl
+++ test_scripts/Introduction_perl.pl
@@ -1,19 +1,23 @@
-use strict;
+use strict;  #enables strict variable declarations, Symbolic Refs and Subs
 use warnings;
 
 $|=1; 											#to put-off output buffering and get immediate outputs
 
+#This is a sub-routine
+#main is just a name. You can call it anything
 sub main {
+#my => represents the first use of the variable
+#files => array of files
 	my @files = ('C:\Users\sravya\Desktop\OPT.jpg',
 				'C:\Users\sravya\Desktop\OPT',
-				'C:\Users\sravya\Desktop\DFT.docx',
+				'C:\Users\sravya\Desktop\DFT.docx', # you can have comma even for the last element unlike other languages
 				);
 	
 	#my $file ='C:\Users\sravya\Desktop\OPT.jpg';
 	
 	foreach my $file (@files){                  #arrays
 		print "Hello World! \n";
-		if(-f $file){                           #checking the presence of file
+		if(-f $file){                           #-f => checking the presence of file
 			print "found file $file \n";
 		}
 		else{
@@ -25,5 +29,3 @@ sub main {
 }
 
 main();
-
-
